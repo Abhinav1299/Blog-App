c4b5eb0160652a81c8d73a714bca5fd9e98d5da0
Abhinav Gupta <abhinavg1211@gmail.com>
2021-11-29 21:16:58 +0530

    updated model testing

--- src/blog/tests.py
+++ src/blog/tests.py
@@ -3,14 +3,15 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import Post, Category
 
+# testing creates posts on dummy database
+
 
 # TESTING BLOG MODELS
-
-# testing create post on dummy database
 class TestCreatPost(TestCase):
 
+    #  method for creating data in database
     @classmethod
-    def setUpTestData(cls):
+    def setUpTestData(self):
         testCategory = Category.objects.create(name = 'django')
         testUser = User.objects.create_user(username = 'testUser', password = '1234')
         testPost = Post.objects.create(category_id = 1, title = 'testpost', 
@@ -18,8 +19,8 @@ class TestCreatPost(TestCase):
             slug = 'testing-post', post_status = 'published')
 
 
-    def testBlogContent(self):
-        
+    # test for checking retrieved data from database
+    def testPostContent(self):        
         post = Post.objects.get(id = 1)
         cat = Category.objects.get(id = 1)
 
