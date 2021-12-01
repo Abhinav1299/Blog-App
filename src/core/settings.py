b95702da9e0a532b363b19bc1669e0d5c10522ad
Abhinav Gupta <abhinavg1211@gmail.com>
2021-12-01 19:28:04 +0530

    added django cors headers

--- src/core/settings.py
+++ src/core/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'blog',
     'blog_api',
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -138,3 +140,8 @@ REST_FRAMEWORK = {
     ]
 
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
